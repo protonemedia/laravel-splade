7f740f18ff47cd4c92be110919c8e04bc76e7c1d
pascalbaljet <pascal@pascalbaljet.nl>
2022-08-02 11:50:48 +0200

    Refactor + run stub tests on Windows

--- src/Commands/SpladeInstallCommand.php
+++ src/Commands/SpladeInstallCommand.php
@@ -72,11 +72,21 @@ class SpladeInstallCommand extends Command
         return self::SUCCESS;
     }
 
+    /**
+     * End of line symbol.
+     *
+     * @return string
+     */
+    private static function eol(): string
+    {
+        return windows_os() ? "\n" : PHP_EOL;
+    }
+
     protected function installExceptionHandler()
     {
-        $exceptionHandler = str_replace("\r\n", "\n", file_get_contents(app_path('Exceptions/Handler.php')));
+        $exceptionHandler = file_get_contents(app_path('Exceptions/Handler.php'));
 
-        $search =  str_replace("\r\n", "\n", 'public function register()' . PHP_EOL . '    {');
+        $search = 'public function register()' . static::eol() . '    {';
 
         $registerMethodAfter = Str::after($exceptionHandler, $search);
 
@@ -90,7 +100,7 @@ class SpladeInstallCommand extends Command
             app_path('Exceptions/Handler.php'),
             str_replace(
                 $registerMethodAfter,
-                PHP_EOL . '        ' . $renderable . PHP_EOL . $registerMethodAfter,
+                static::eol() . '        ' . $renderable . static::eol() . $registerMethodAfter,
                 $exceptionHandler
             )
         );
@@ -103,13 +113,13 @@ class SpladeInstallCommand extends Command
      */
     protected function installRouteMiddleware()
     {
-        $httpKernel = str_replace("\r\n", "\n", file_get_contents(app_path('Http/Kernel.php')));
+        $httpKernel = file_get_contents(app_path('Http/Kernel.php'));
 
-        $search = str_replace("\r\n", "\n", 'protected $routeMiddleware = [' . PHP_EOL);
+        $search = 'protected $routeMiddleware = [' . static::eol();
 
         $routeMiddlewareAfter = Str::after($httpKernel, $search);
 
-        $routeMiddleware = "'splade'           => \ProtoneMedia\Splade\Http\SpladeMiddleware::class";
+        $routeMiddleware = "'splade' => \ProtoneMedia\Splade\Http\SpladeMiddleware::class";
 
         if (Str::contains($httpKernel, $routeMiddleware)) {
             return;
@@ -119,7 +129,7 @@ class SpladeInstallCommand extends Command
             app_path('Http/Kernel.php'),
             str_replace(
                 $routeMiddlewareAfter,
-                '        ' . $routeMiddleware . ',' . PHP_EOL . $routeMiddlewareAfter,
+                '        ' . $routeMiddleware . ',' . static::eol() . $routeMiddlewareAfter,
                 $httpKernel
             )
         );
@@ -165,7 +175,7 @@ class SpladeInstallCommand extends Command
 
         file_put_contents(
             base_path('package.json'),
-            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . PHP_EOL
+            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . static::eol()
         );
     }
 }
