cc3044f6b4997e6c3b47a449121780d86d93f62c
Pascal Baljet <pascal@pascalbaljet.nl>
2022-08-22 18:50:49 +0200

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,11 +1,13 @@
 @php $data = $formData() @endphp
 
-<SpladeForm {!! $attributes !!}
+<SpladeForm {!! $attributes->except('class') !!}
     @if($data['data']) :default="@js($data['data'])" @else :default="{!! $data['json'] !!}" @endif
 >
     <template #default="{!! $scope !!}">
         <form v-bind="form.$attrs" @submit.prevent="form.submit">
-            {{ $slot }}
+            <fieldset v-bind:disabled="form.processing" {!! $attributes->only('class') !!}>
+                {{ $slot }}
+            </fieldset>
         </form>
     </template>
 </SpladeForm>
