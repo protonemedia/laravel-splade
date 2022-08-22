ef1b2697151e4f50778aad6ae9db78f5ee57028c
pascalbaljet <pascal@pascalbaljet.nl>
2022-08-22 11:13:09 +0200

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,7 +1,7 @@
 @php $data = $formData() @endphp
 
 <SpladeForm {!! $attributes !!}
-    @if($data['data']) :default="@js($data['data'])" @else :default="{!! $data['json'] !!} @endif
+    @if($data['data']) :default="@js($data['data'])" @else :default="{!! $data['json'] !!}" @endif
 >
     <template #default="{!! $scope !!}">
         <form v-bind="form.$attrs" @submit.prevent="form.submit">
