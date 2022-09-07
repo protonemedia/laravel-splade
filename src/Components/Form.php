a7c2919bbf07fbf0cdedfeeba9162a4322e6d1f2
Pascal Baljet <pascal@pascalbaljet.nl>
2022-09-07 09:35:25 +0200

    Update Form.php

--- src/Components/Form.php
+++ src/Components/Form.php
@@ -6,7 +6,6 @@ use Closure;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Fluent;
 use Illuminate\Support\Str;
@@ -146,10 +145,6 @@ class Form extends Component
         $guardedData = [];
 
         foreach (static::allowedAttributesSorted() as $attribute) {
-            if (!Arr::has($rawData, $attribute)) {
-                continue;
-            }
-
             data_set($guardedData, $attribute, data_get($rawData, $attribute));
         }
 
