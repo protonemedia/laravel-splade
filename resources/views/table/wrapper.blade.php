4739a1f89ba2f0c84aa541155ad01cabd742b223
Pascal Baljet <pascal@pascalbaljet.nl>
2022-09-28 21:34:20 +0200

    Table UI

--- resources/views/table/wrapper.blade.php
+++ resources/views/table/wrapper.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col mt-3">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="shadow border-b border-gray-200 relative">
+            <div class="shadow relative rounded-md overflow-hidden">
                 {{ $slot }}
             </div>
         </div>
