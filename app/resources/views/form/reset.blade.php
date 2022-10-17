f0930fa528c09b7ea9bb8931119a416a2b6d823d
pascalbaljet <pascal@pascalbaljet.nl>
2022-10-17 09:37:03 +0200

    Forced stay

--- app/resources/views/form/reset.blade.php
+++ app/resources/views/form/reset.blade.php
@@ -7,7 +7,7 @@ FormReset
 <x-splade-form
     stay
     reset-on-success
-    :action="route('form.back.submit')">
+    :action="route('form.simple.submit')">
     <input v-model="form.name" dusk="name" />
     <button type="submit">Submit</button>
     <p v-if="form.wasSuccessful">Success!</p>
