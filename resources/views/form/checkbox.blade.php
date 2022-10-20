37c0a27fa63f05631ec0001a0b6c65b0f4254811
pascalbaljet <pascal@pascalbaljet.nl>
2022-10-20 18:22:42 +0200

    WIP

--- resources/views/form/checkbox.blade.php
+++ resources/views/form/checkbox.blade.php
@@ -1,15 +1,17 @@
 <div {{ $attributes->only(['v-if', 'v-show', 'class']) }}>
     <label class="flex items-center">
-        <input {{ $attributes->except(['v-if', 'v-show', 'class'])->class(
-            'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:opacity-50'
-        )->merge([
-            'name' => $name,
-            'value' => $value,
-            'type' => 'checkbox',
-            'v-model' => $vueModel(),
-            'data-validation-key' => $validationKey(),
-        ]) }}
-        />
+        <input {{ $attributes->except(['v-if', 'v-show', 'class'])
+            ->class('rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:opacity-50')
+            ->merge([
+                'name' => $name,
+                'value' => $value,
+                'type' => 'checkbox',
+                'v-model' => $vueModel(),
+                'data-validation-key' => $validationKey(),
+            ])
+            ->mergeVueBinding(':true-value', $value)
+            ->mergeVueBinding(':false-value', $falseValue)
+        }} />
 
         @if(trim($slot))
             <span class="ml-2">{{ $slot }}</span>
