c8fd793ba51df19010d6a2e42d12833d8da964ff
Pascal Baljet <pascal@pascalbaljet.nl>
2022-11-14 23:06:48 +0100

    Select filter improvements

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -20,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('language_code');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
