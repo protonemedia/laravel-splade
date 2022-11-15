d4d40cea9879d18359fb4e1b88d5891e4755b0ca
Pascal Baljet <pascal@pascalbaljet.nl>
2022-11-15 14:15:40 +0100

    Update SortTest.php

--- app/tests/Browser/Table/SortTest.php
+++ app/tests/Browser/Table/SortTest.php
@@ -31,21 +31,24 @@ class SortTest extends DuskTestCase
                 ->assertSeeIn('tr:last-child td:nth-child(1)', $users->get(9)->name)
 
                 // Sort desc
+                ->pause(250)
                 ->click('@sort-name')
                 ->waitUntilMissingText($users->get(0)->name)
                 ->assertSeeIn('tr:first-child td:nth-child(1)', $users->get(99)->name)
                 ->assertSeeIn('tr:last-child td:nth-child(1)', $users->get(90)->name)
 
                 // Restore asc sort
+                ->pause(250)
                 ->click('@sort-name')
                 ->waitUntilMissingText($users->get(99)->name)
                 ->assertSeeIn('tr:first-child td:nth-child(1)', $users->get(0)->name)
                 ->assertSeeIn('tr:last-child td:nth-child(1)', $users->get(9)->name)
 
                 // Sort by other column
+                ->pause(250)
                 ->click('@sort-email')
-                ->waitForTextIn('tr:first-child td:nth-child(2)', $usersByEmail->get(0)->email)
                 ->waitForText($usersByEmail->get(9)->email)
+                ->waitForTextIn('tr:first-child td:nth-child(2)', $usersByEmail->get(0)->email)
                 ->assertSeeIn('tr:first-child td:nth-child(2)', $usersByEmail->get(0)->email)
                 ->assertSeeIn('tr:last-child td:nth-child(2)', $usersByEmail->get(9)->email);
         });
