2e1ff5150aec06768405b34d1792ab3f6fef2959
pascalbaljet <pascalbaljet@users.noreply.github.com>
2022-11-19 10:19:00 +0000

    Fix styling

--- config/splade.php
+++ config/splade.php
@@ -3,30 +3,35 @@
 return [
     /**
      * The URI to handle Event Redirects.
+     *
      * @see https://splade.dev/docs/x-event
      */
     'event_redirect_route' => '/_splade/eventRedirect/{uuid}',
 
     /**
      * URI to handle Table Bulk Actions.
+     *
      * @see https://splade.dev/docs/table-bulk-actions
      */
     'table_bulk_action_route' => '/_splade/table/action/{table}/{action}/{slug}',
 
     /**
      * The URI to handle Table Export Actions.
+     *
      * @see https://splade.dev/docs/table-exports
      */
     'table_export_route' => '/_splade/table/export/{table}/{export}/{slug}',
 
     /**
      * Share all flash data to the Vue front-end app.
+     *
      * @see https://splade.dev/docs/x-flash
      */
     'share_session_flash_data' => true,
 
     /**
      * Temporary directory for file uploads with Filepond.
+     *
      * @see https://splade.dev/docs/form-file
      */
     'file_uploads' => [
@@ -37,6 +42,7 @@ return [
 
     /**
      * Settings for the Splade Blade components.
+     *
      * @see https://splade.dev/docs/form-overview
      */
     'blade' => [
@@ -46,6 +52,7 @@ return [
 
     /**
      * Server-side rendering (SSR) settings.
+     *
      * @see https://splade.dev/docs/ssr
      */
     'ssr' => [
@@ -56,6 +63,7 @@ return [
 
     /**
      * Names of the Dusk macro to interact with Choices.js instances.
+     *
      * @see https://splade.dev/docs/form-select
      */
     'dusk' => [
