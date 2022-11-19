ac04c7fc5f639e15a9b5d4bbedbfbea4706a6489
Pascal Baljet <pascal@pascalbaljet.nl>
2022-11-19 11:30:50 +0100

    Command to show Splade version

---
+++ src/Commands/ShowSpladeVersions.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ProtoneMedia\Splade\Commands;
+
+use Illuminate\Console\Command;
+
+class ShowSpladeVersions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'splade:show-versions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show the composer and npm versions of the Splade packages';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $composerLockFile = $this->get(base_path('composer.lock'));
+        $npmLockFile      = $this->get(base_path('package-lock.json'));
+
+        $composerPackage = collect($composerLockFile['packages'] ?? [])
+            ->merge($composerLockFile['packages-dev'] ?? [])
+            ->firstWhere('name', 'protonemedia/laravel-splade');
+
+        $npmPackage = collect($npmLockFile['packages'] ?? [])
+            ->merge($npmLockFile['dependencies'] ?? [])
+            ->first(function (array $value, string $name) {
+                return $name === '@protonemedia/laravel-splade';
+            })['version'] ?? null;
+
+        $composerPackage
+            ? $this->info("Composer (PHP) package version: " . $composerPackage['version'])
+            : $this->error('Composer (PHP) package not found');
+
+        $npmPackage
+            ? $this->info("NPM (JS) package version: {$npmPackage}")
+            : $this->error('NPM (JS) package not found');
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * Get the contents of a file.
+     *
+     * @param string $path
+     * @return array
+     */
+    private function get($path): array
+    {
+        if (!file_exists($path)) {
+            return [];
+        }
+
+        $contents = file_get_contents($path);
+
+        return json_decode($contents, true) ?: [];
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -15,6 +15,7 @@ use Illuminate\View\Factory;
 use Laravel\Dusk\Browser;
 use ProtoneMedia\Splade\Commands\CleanupTemporaryFileUploads;
 use ProtoneMedia\Splade\Commands\PublishFormStylesheetsCommand;
+use ProtoneMedia\Splade\Commands\ShowSpladeVersions;
 use ProtoneMedia\Splade\Commands\SpladeInstallCommand;
 use ProtoneMedia\Splade\Commands\SsrTestCommand;
 use ProtoneMedia\Splade\Commands\TableMakeCommand;
@@ -59,6 +60,7 @@ class ServiceProvider extends BaseServiceProvider
         $this->commands([
             CleanupTemporaryFileUploads::class,
             PublishFormStylesheetsCommand::class,
+            ShowSpladeVersions::class,
             SpladeInstallCommand::class,
             SsrTestCommand::class,
             TableMakeCommand::class,
