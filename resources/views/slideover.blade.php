3bd9b730e71195dc8acb74cbb5f04e3a9966989b
Pascal Baljet <pascal@pascalbaljet.nl>
2022-11-22 13:45:58 +0100

    Modal/slideover class

--- resources/views/slideover.blade.php
+++ resources/views/slideover.blade.php
@@ -1,4 +1,4 @@
-<x-splade-component is="modal-wrapper" :base-attributes="$attributes" :key="$modalKey" :close-button="$closeButton" :name="$name">
+<x-splade-component is="modal-wrapper" :base-attributes="$attributes->except('class')" :key="$modalKey" :close-button="$closeButton" :name="$name">
     <!-- Full-screen scrollable container -->
     <div class="fixed inset-0 z-40 overflow-y-auto">
         <!-- Container to center the panel -->
@@ -19,7 +19,7 @@
                     'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl': modal.maxWidth == '7xl'
                 }">
                 <x-splade-component is="dialog" panel dusk="slideover-dialog">
-                    <div class="bg-white p-6 min-h-screen relative">
+                    <div {{ $attributes->class('bg-white p-6 rounded relative') }}>
                         <div v-if="modal.closeButton" class="absolute top-0 right-0 pt-3 pr-3">
                             <button dusk="close-modal-button" @click="modal.close" type="button" class="text-gray-400 hover:text-gray-500">
                                 <span class="sr-only">Close</span>
