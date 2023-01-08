398f7b28aa8b295e0510f3ef67bf04f83d4e8c3e
Pascal Baljet <pascal@pascalbaljet.nl>
2023-01-08 22:21:42 +0100

    Translations (#196)

---
+++ app/config/translator.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Translator\Framework\LaravelConfigLoader;
+use Translator\Infra\LaravelJsonTranslationRepository;
+
+return [
+    'languages'   => ['en'],
+    'directories' => [
+        base_path('../resources/views'),
+        base_path('../src'),
+    ],
+    'output'      => base_path('../resources/lang'),
+    'extensions'  => ['php'],
+    'container'   => [
+        'config_loader'          => LaravelConfigLoader::class,
+        'translation_repository' => LaravelJsonTranslationRepository::class,
+    ],
+];
