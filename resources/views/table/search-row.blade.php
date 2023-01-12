782512b5ae28c8aa9111a8a84b57916f97487611
Aleskandr Shatskikh <work@errand.ru>
2023-01-12 14:13:04 +0300

    refactor: localization wrappers __() for the Reset and Remove search … (#215)
    Co-authored-by: pascalbaljet <pascal@pascalbaljet.nl>

--- resources/views/table/search-row.blade.php
+++ resources/views/table/search-row.blade.php
@@ -30,7 +30,7 @@
                 @click.prevent="table.disableSearchInput(@js($searchInput->key))"
                 dusk="remove-search-row-{{ $searchInput->key }}"
             >
-                <span class="sr-only">Remove search</span>
+                <span class="sr-only">{{ __('Remove search') }}</span>
 
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
