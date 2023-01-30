74c4ad520c222fe751fb8c54edb30a8586b20510
Pascal Baljet <pascal@pascalbaljet.nl>
2023-01-30 20:22:25 +0100

    Bulk Action password confirmation + Bugfix for #266 (#272)

--- app/tests/Browser/Table/BulkActionTest.php
+++ app/tests/Browser/Table/BulkActionTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Browser\Table;
 
 use App\Models\Project;
+use App\Models\User;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
@@ -68,6 +69,68 @@ class BulkActionTest extends DuskTestCase
                 ->waitForText('Timestamps updated!');
 
             $this->assertEquals(15, Project::whereUpdatedAt($someTimeAgo)->count());
+        });
+    }
+
+    /** @test */
+    public function it_can_perform_one_all_rows_on_the_current_page_with_a_password_confirmation()
+    {
+        $this->browse(function (Browser $browser) {
+            $someTimeAgo = now()->subDecade();
+
+            $this->assertEquals(30, Project::count());
+
+            Project::withoutTouching(function () use ($someTimeAgo) {
+                Project::query()->update(['updated_at' => $someTimeAgo]);
+            });
+
+            $browser->loginAs(User::firstOrFail())
+                ->visit('table/relationsAndExports')
+                ->press('@select-rows-dropdown')
+                ->press('@select-all-on-this-page')
+
+                ->press('@bulk-actions-exports-dropdown')
+                ->press('@action.touch-timestamp-confirm-password')
+                ->waitForText('Are you sure you want to continue?')
+                ->type('password', 'password')
+                ->press('@splade-confirm-confirm')
+                ->waitForText('Timestamps updated!');
+
+            $this->assertEquals(15, Project::whereUpdatedAt($someTimeAgo)->count());
+        });
+    }
+
+    /** @test */
+    public function it_can_perform_on_all_rows_of_the_filtered_result()
+    {
+        $this->browse(function (Browser $browser) {
+            $someTimeAgo = now()->subDecade();
+
+            $this->assertEquals(30, Project::count());
+
+            Project::withoutTouching(function () use ($someTimeAgo) {
+                Project::query()->update(['updated_at' => $someTimeAgo]);
+            });
+
+            $projects = Project::orderBy('name')->get();
+
+            /** @var Project */
+            $firstProject = $projects->first();
+            $firstProject->forceFill(['name' => 'aaaaaa'])->save();
+
+            $browser->visit('table/relationsAndExports')
+                ->type('searchInput-global', $firstProject->name)
+                ->waitUntilMissingText($projects->get(1)->name)
+                ->assertSee($firstProject->name)
+                ->press('@select-rows-dropdown')
+                ->press('@select-all-results')
+                ->press('@bulk-actions-exports-dropdown')
+                ->pause(100)
+                ->press('@action.touch-timestamp')
+                ->pause(100)
+                ->waitForText('Timestamps updated!');
+
+            $this->assertEquals(29, Project::whereUpdatedAt($someTimeAgo)->count());
         });
     }
 
--- src/Http/ConfirmPasswordController.php
+++ src/Http/ConfirmPasswordController.php
@@ -2,71 +2,22 @@
 
 namespace ProtoneMedia\Splade\Http;
 
-use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\ValidationException;
-use Laravel\Fortify\Actions\ConfirmPassword;
+use ProtoneMedia\Splade\PasswordValidator;
 
 class ConfirmPasswordController
 {
     /**
-     * Confirm the given user's password using Laravel Fortify.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    private function confirmWithFortify(Request $request): bool
-    {
-        return app(ConfirmPassword::class)(
-            app(StatefulGuard::class), $request->user(), $request->input('password')
-        );
-    }
-
-    /**
-     * Confirm the given user's password using regular Laravel Auth logic.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    private function confirmWithoutForfify(Request $request): bool
-    {
-        $guard = Auth::guard(config('splade.confirm_password_guard'));
-
-        $user = $guard->user();
-
-        return $guard->validate([
-            $user->getAuthIdentifierName() => $user->getAuthIdentifier(),
-            'password'                     => $request->input('password'),
-        ]);
-    }
-
-    /**
      * Confirm the given user's password.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \ProtoneMedia\Splade\PasswordValidator  $passwordValidator
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, PasswordValidator $passwordValidator): Response
     {
-        $user = $request->user();
-
-        if (!$user) {
-            throw ValidationException::withMessages([
-                'password' => __('No user is logged in'),
-            ]);
-        }
-
-        $confirmed = class_exists(ConfirmPassword::class)
-            ? $this->confirmWithFortify($request)
-            : $this->confirmWithoutForfify($request);
-
-        if (!$confirmed) {
-            throw ValidationException::withMessages([
-                'password' => __('auth.password'),
-            ]);
-        }
+        $passwordValidator->validateRequest($request, 'password');
 
         $request->session()->put('auth.password_confirmed_at', time());
 
