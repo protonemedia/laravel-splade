ca1e98fbad7f0d9d2554c1c04e3dbba83d9daa2a
Pascal Baljet <pascal@pascalbaljet.nl>
2023-02-09 14:42:14 +0100

    Fix for custom label slot in checkboxes/radios components

--- resources/views/form/radios.blade.php
+++ resources/views/form/radios.blade.php
@@ -1,4 +1,6 @@
-<x-splade-component is="group" :name="$name" :label="$label" :inline="$inline" :help="$help" {{ $attributes }}>
+<x-splade-component is="group" :name="$name" :inline="$inline" :help="$help" {{ $attributes }}>
+    <x-slot:label>{{ $label }}</x-slot:label>
+
     @foreach($options as $value => $label)
         <x-splade-component is="radio" :name="$name" :label="$label" :value="$value" />
     @endforeach
