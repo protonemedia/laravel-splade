4f9c2eaacb6e9b3278df271a2308ee4f80a7970f
Pascal Baljet <pascal@pascalbaljet.nl>
2023-03-08 11:54:20 +0100

    Update wrapper.blade.php

--- resources/views/table/wrapper.blade.php
+++ resources/views/table/wrapper.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col mt-3">
+<div class="flex flex-col">
     <div class="-my-2 overflow-x-auto">
         <div class="py-2 align-middle inline-block min-w-full sm:px-px">
             <div class="shadow-sm relative border border-gray-200 sm:rounded-md sm:overflow-hidden">
