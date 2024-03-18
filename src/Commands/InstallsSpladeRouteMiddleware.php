dffa10b8175aea1482fa8bdd117ce85b4da9aef1
Pascal Baljet <pascal@protone.media>
2024-03-18 15:32:49 +0100

    Support for installing on fresh L11 installations (#557)

--- src/Commands/InstallsSpladeRouteMiddleware.php
+++ src/Commands/InstallsSpladeRouteMiddleware.php
@@ -13,13 +13,44 @@ trait InstallsSpladeRouteMiddleware
      */
     protected function installRouteMiddleware()
     {
+        version_compare(app()->version(), '11.0', '<')
+            ? $this->installRouteMiddlewareForLegacyLaravelSkeleton()
+            : $this->installRouteMiddlewareForModernLaravelSkeleton();
+    }
+
+    protected function installRouteMiddlewareForModernLaravelSkeleton()
+    {
+        $appBoostrap = file_get_contents(base_path('bootstrap/app.php'));
+
+        $eol = SpladeInstallCommand::eol();
+
+        $search = '->withMiddleware(function (Middleware $middleware) {';
+
+        $routeMiddleware = '$middleware->group(\'splade\', [\ProtoneMedia\Splade\Http\SpladeMiddleware::class]);';
+
+        $middlewareAfter = Str::after($appBoostrap, $search);
+
+        if (Str::contains($appBoostrap, $routeMiddleware)) {
+            return;
+        }
+
+        file_put_contents(
+            base_path('bootstrap/app.php'),
+            str_replace(
+                $middlewareAfter,
+                $eol . '        ' . $routeMiddleware . $middlewareAfter,
+                $appBoostrap
+            )
+        );
+    }
+
+    protected function installRouteMiddlewareForLegacyLaravelSkeleton()
+    {
         $httpKernel = file_get_contents(app_path('Http/Kernel.php'));
 
         $eol = SpladeInstallCommand::eol();
 
-        $search = version_compare(app()->version(), '10.0', '>=')
-            ? 'protected $middlewareAliases = [' . $eol
-            : 'protected $routeMiddleware = [' . $eol;
+        $search = 'protected $middlewareAliases = [' . $eol;
 
         $routeMiddlewareAfter = Str::after($httpKernel, $search);
 
